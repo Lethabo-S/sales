9868ba01523559c468319effc90d3be50472220d
Lethabo-S <lethabosuprise@outlook.com>
2025-07-22 13:24:17 +0200

    Update firebaseConfig.ts

--- project/src/firebaseConfig.ts
+++ project/src/firebaseConfig.ts
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
- apiKey: "AIzaSyCcc-4VaNgsDpjPvxOCyaPZI_wNubaleTQ",
-  authDomain: "my-sales-chatbot.firebaseapp.com",
-  projectId: "my-sales-chatbot",
-  storageBucket: "my-sales-chatbot.firebasestorage.app",
-  messagingSenderId: "594096587386",
-  appId: "1:594096587386:web:b4a0bfdb66c52188d4b1c3",
-  measurementId: "G-7362MXJXWR"
+
 };
 
 const app = initializeApp(firebaseConfig);
